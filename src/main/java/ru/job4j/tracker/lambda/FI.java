23efef978b25c5b93e3f4bc02e9f6882dcd31246
denisRudie <moonstamp1964@gmail.com>
2020-07-14 00:47:39 +0300

    2.1. Лямбда [#325844]

--- src/main/java/ru/job4j/tracker/lambda/FI.java
+++ src/main/java/ru/job4j/tracker/lambda/FI.java
@@ -1,5 +1,6 @@
 package ru.job4j.tracker.lambda;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -34,5 +35,19 @@ public class FI {
 
         Mark m = markFunction.apply(students.get(0));
         System.out.println(m);
+
+//        Comparators for sorting
+        Comparator<String> cmpText = (left, right) -> left.compareTo(right);
+        Comparator<String> cmpDescSize = (left, right) -> left.length() - right.length();
+
+//        testing comparators
+        String[] strings1 = {"World!", "Hello", "abc"};
+        Arrays.sort(strings1, cmpDescSize);
+        System.out.println(Arrays.toString(strings1));
+
+        String[] strings2 = {"World!", "Hello", "Java"};
+        Arrays.sort(strings2, cmpText);
+        System.out.println(Arrays.toString(strings2));
+
     }
 }
