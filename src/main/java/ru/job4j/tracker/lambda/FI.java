4aa6a45cd9206e320876e58544bfc56732180278
denisRudie <moonstamp1964@gmail.com>
2020-07-14 01:13:47 +0300

    2.2. Лямбда блок [#325849]

--- src/main/java/ru/job4j/tracker/lambda/FI.java
+++ src/main/java/ru/job4j/tracker/lambda/FI.java
@@ -38,7 +38,10 @@ public class FI {
 
 //        Comparators for sorting
         Comparator<String> cmpText = (left, right) -> left.compareTo(right);
-        Comparator<String> cmpDescSize = (left, right) -> right.length() - left.length();
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + left.length() + " : " + right.length());
+            return right.length() - left.length();
+        };
 
 //        testing comparators
         String[] strings1 = {"World!", "Hello", "abc"};
