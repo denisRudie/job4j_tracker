5b352fe5f4d3116b082f6ce80cda571112383498
denisRudie <moonstamp1964@gmail.com>
2020-07-14 20:47:28 +0300

    4. Зона видимости в лямбда-выражениях и исключения [#325846] fixes error with updating var inside lambda

--- src/main/java/ru/job4j/tracker/lambda/ScopeInside.java
+++ src/main/java/ru/job4j/tracker/lambda/ScopeInside.java
@@ -6,11 +6,17 @@ public class ScopeInside {
     public static void main(String[] args) {
         int[] number = {1, 2, 3};
         int total = 0;
+
         for (int i = 0; i < number.length; i++) {
             int num = number[i];
-            total += num;
+            total += add(
+                    () -> num
+            );
         }
         System.out.println(total);
     }
 
+    private static Integer add(Supplier<Integer> calc) {
+        return calc.get();
+    }
 }
