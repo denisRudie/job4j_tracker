0d7cc6ce20e47c6a535c6a8f0f764c44c0368f09
denisRudie <moonstamp1964@gmail.com>
2020-07-16 10:08:11 +0300

    5. Преобразование матрицы чисел в список чисел. [#325861] add access modifiers in test class

--- src/test/java/ru/job4j/tracker/stream/FlatMapConverterTest.java
+++ src/test/java/ru/job4j/tracker/stream/FlatMapConverterTest.java
@@ -8,16 +8,18 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 
 public class FlatMapConverterTest {
-    FlatMapConverter fmc = new FlatMapConverter();
 
-    Integer[][] matrix = {
+    private FlatMapConverter fmc = new FlatMapConverter();
+
+    private Integer[][] matrix = {
             {1, 2},
             {3, 4},
             {5, 6},
             {}
     };
 
-    List<Integer> model = List.of(1, 2, 3, 4, 5, 6);
+    private List<Integer> model = List.of(1, 2, 3, 4, 5, 6);
+
     @Test
     public void ListContainsArray() {
         List<Integer> rsl = fmc.convert(matrix);
